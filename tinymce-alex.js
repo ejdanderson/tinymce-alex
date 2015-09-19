bead48cc96fb385ca366571bd9bdada7032288d7
Evan Anderson <kdfiction@gmail.com>
2015-09-19 16:00:47 -0600

    Refactor code, create plugin as a tinyMCE class

--- tinymce-alex.js
+++ tinymce-alex.js
@@ -1,14 +1,40 @@
+//TODO Don't rely on jQuery
 (function($) {
-	tinymce.PluginManager.add('alextinymce', function(editor, url) {
-		// On content load run alex
-		// TODO
+	tinymce.create('tinymce.plugins.alextinymce', {
+		init : function(editor, url) {
+			var self = this;
+			self.editor = editor;
 
-		// On node change, run alex
-		editor.on('change', function(e) {
-			var content = editor.getContent();
+			// TODO
+			// On content load run alex
+
+			editor.on('init', function(e){
+				// parentElement.parentElement.parentElement is WP Specific, need to insert after wordcount etc...
+				self.$alexWrapper = $('<div class="alex-tinymce-wrapper"></div>').insertAfter(editor.container);
+			});
+
+			// On node change, run alex
+			editor.on('change', function(e) {
+				self.runAlex();
+			});
+		},
+
+		runAlex : function() {
+			var content = this.editor.getContent();
 			var rawText = $(content).text();
+			var html = this.formatMessages(alex(rawText).messages);
+			this.$alexWrapper.html(html);
+		},
 
-			console.log(alex(rawText).messages);
-		});
+		formatMessages : function(messages) {
+			var html = '';
+			for (var i = 0; i <= messages.length - 1; i++) {
+				 html += messages[i].message + '<br />';
+			};
+
+			return html;
+		}
 	});
+
+	tinymce.PluginManager.add('alextinymce', tinymce.plugins.alextinymce);
 })(jQuery);
