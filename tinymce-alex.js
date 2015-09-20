8d43a3032e5e4f3acccf58bc50cf7d8c97716047
Evan Anderson <kdfiction@gmail.com>
2015-09-20 03:33:48 -0600

    Overall basic use case, adding to README

--- tinymce-alex.js
+++ tinymce-alex.js
@@ -1,61 +1,115 @@
-//TODO Don't rely on jQuery
-(function($) {
-	// Load Alex Dependency
-	tinymce.ScriptLoader.load( tinymce.PluginManager.urls.alextinymce + '/alex/alex.js');
+// Load Alex Dependency
+tinymce.ScriptLoader.load(tinymce.PluginManager.urls.alextinymce + '/alex/alex.js');
 
-	// Create tinyMCE Class
-	tinymce.create('tinymce.plugins.alextinymce', {
-		init : function(editor, url) {
-			var self = this;
-			self.editor = editor;
+// Create tinyMCE Class
+tinymce.create('tinymce.plugins.alextinymce', {
+	init : function(editor, url) {
+		var self = this;
+		self.editor = editor;
 
-			// Load CSS
-			tinymce.DOM.loadCSS(url + '/tinymce-alex.css');
+		// fake jQuery
+		self.$ = tinymce.dom.DomQuery;
 
-			// TODO
-			// On content load, run alex
+		// Load CSS
+		tinymce.DOM.loadCSS(url + '/tinymce-alex.css');
 
-			editor.on('init', function(e){
-				var $wrapper = $('<div id="alex-tinymce-wrapper"><div id="alex-tinymce-handle"> > AlexJS</div></div>').insertAfter(editor.container);
-				self.$messageWrapper = $('<div id="alex-tinymce-innerwrapper"></div>').appendTo($wrapper);
-			});
+		editor.on('init', function(e){
+			var $wrapper;
+			self.$(editor.container).after('<div id="mce-alex-wrapper"><div id="mce-alex-handle"> <span class="mce-alex-toggle">-</span> AlexJS : <span id="mce-alex-count">0</div></div>');
+			$wrapper = self.$('#mce-alex-wrapper');
 
-			// On node change, run alex
-			editor.on('change', function(e) {
-				self.runAlex();
-			});
-		},
+			self.$countWrapper = self.$('#mce-alex-count');
+			self.$messageWrapper = self.$('<ul id="mce-alex-ul"></ul>').appendTo($wrapper);
+			self.rootNode = self.editor.dom.getRoot();
+			self.$('#mce-alex-handle').on('click', function(e) {
 
-		runAlex : function() {
-			var content = this.editor.getContent();
-			var rawText = $(content).text();
-			var html = this.formatMessages(alex(rawText).messages);
-			console.log(alex(rawText).messages);
-			this.$messageWrapper.html(html);
-		},
-
-		formatMessages : function(messages) {
-			var html = '';
-			for (var i = 0; i <= messages.length - 1; i++) {
-				 html += this.addMessageClass(messages[i]) + '<br />';
-			};
-
-			return html;
-		},
-
-		addMessageClass : function(message) {
-			var splitIndex = message.message.indexOf('insensitive'); // insensitive shouldn't be matched
-
-			return message.message.replace(/`(.+?)`/g, function (match, capture, offset) {
-				var messageClass = 'mce-alex-sensitive';
-				if ( offset < splitIndex ) {
-					messageClass = 'mce-alex-insensitive';
+				if (self.$messageWrapper.hasClass('mce-alex-hide') ) {
+					self.$messageWrapper.removeClass('mce-alex-hide');
+					self.$('.mce-alex-toggle').html('-');
+				}
+				else {
+					self.$messageWrapper.addClass('mce-alex-hide');
+					self.$('.mce-alex-toggle').html('+');
 				}
 
-				return '<code class="' + messageClass + '">' + capture + '</code>';
 			});
-		}
-	});
+		});
 
-	tinymce.PluginManager.add('alextinymce', tinymce.plugins.alextinymce);
-})(jQuery);
+		// On init, run alex (runs once)
+		editor.on('init', function(e) {
+			self.runAlex();
+		});
+
+		// On node change, run alex. TODO run this on keyup event with debounce implementation
+		editor.on('change', function(e) {
+			self.runAlex();
+		});
+
+
+	},
+
+	runAlex : function(e) {
+		var rawText = this.rootNode.innerText;
+		var messages = alex(rawText).messages;
+		var html = this.formatMessages(messages);
+
+		if ( messages.length == 0) {
+			this.$countWrapper.addClass('mce-alex-zero');
+		}
+		else {
+			this.$countWrapper.removeClass('mce-alex-zero');
+		}
+		this.$countWrapper.html(messages.length);
+		this.$messageWrapper.html(html);
+	},
+
+	/*
+	walkTree : function() {
+		// first remove all bogus wrappers which drive formatting
+		// walk all text nodes
+		// find first instance of word, continue
+			advanced = !! parseInt( window.getUserSetting( 'hidetb' ), 10 ),
+		var walker = document.createTreeWalker(this.rootNode, NodeFilter.SHOW_TEXT, null, false);
+		var node;
+		var results = [];
+
+		while(node = walker.nextNode()) {
+		    results.push(node);
+		}
+		for (var i = 0; i <= results.length - 1; i++) {
+			dummyNode = this.editor.dom.create('div', {id : 'test', 'class' : 'myclass'}, 'some content');
+			this.editor.dom.insertAfter(dummyNode, results[i]);
+			this.editor.dom.remove(results[i]);
+		};
+
+		//TODO implement this with debounce and keyup : http://davidwalsh.name/function-debounce
+		editor.on( 'nodechange keyup', _.debounce( update, 1000 ) );
+		$content.on( 'input keyup', _.debounce( update, 1000 ) );
+
+	},
+	*/
+
+	formatMessages : function(messages) {
+		var html = '';
+		for (var i = 0; i <= messages.length - 1; i++) {
+			 html += '<li>' + this.formatMessage(messages[i]) + '<li>';
+		};
+
+		return html;
+	},
+
+	formatMessage : function(message) {
+		var splitIndex = message.message.indexOf('insensitive'); // insensitive shouldn't be matched
+
+		return message.message.replace(/`(.+?)`/g, function (match, capture, offset) {
+			var messageClass = 'mce-alex-sensitive';
+			if ( offset < splitIndex ) {
+				messageClass = 'mce-alex-insensitive';
+			}
+
+			return '<code class="' + messageClass + '">' + capture + '</code>';
+		});
+	}
+});
+
+tinymce.PluginManager.add('alextinymce', tinymce.plugins.alextinymce);
